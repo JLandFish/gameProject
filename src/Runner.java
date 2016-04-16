7ae635d0c33a43b31990e22f55777a6eca6eaf85
Jessus Alkutby <JessusALK@Gmail.com>
2016-04-16 09:40:56 -0700

    check

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ public class Runner {
 	public static void main(String [] args) throws InterruptedException
 	{
 		
-	
+	System.out.println("hey");
     Warrior Pink_Guy = new Warrior(1000, 25, (int)(Math.random() * 20 + 1), "Pink Guy", "Normal");
     Warrior Jeremy = new Warrior(1000, 25, (int)(Math.random() * 20 + 1), "Weeaboo", "Weeaboo");
     System.out.println("The " + Pink_Guy.getType() + " Pink Guy has " + Pink_Guy.getHP() + " HP, " + Pink_Guy.getMP() + " MP, and deals " + Pink_Guy.getDMG() + " damage");
